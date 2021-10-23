d8282b105bf920d2be488630e5f7bf9e2474dbaa
ssegundo <sebastian.segundo.d@uni.pe>
2021-10-22 19:43:42 -0500

    Agregando los primeros slots

--- client.cpp
+++ client.cpp
@@ -1,15 +1,33 @@
 #include "client.h"
 #include "ui_client.h"
 
-Client::Client(QWidget *parent)
+Client::Client(QWidget *parent) //constructor
     : QWidget(parent)
     , ui(new Ui::Client)
 {
     ui->setupUi(this);
+    connect(ui->pBtn_Close, &QAbstractButton::clicked, this, &QWidget::close);
+    //Procesar datos recibidos
+    connect(clientSocket, &QIODevice::readyRead, this, &Client::readMessage);
+
 }
 
-Client::~Client()
+Client::~Client()   //destructor
 {
     delete ui;
 }
 
+void Client::requestMessage()
+{
+    qDebug() <<"requestMessage() start ...";
+}
+
+void Client::readMessage()
+{
+    qDebug() <<"readMessage() start ...";
+}
+
+void Client::showError()
+{
+    qDebug() <<"showError() start ...";
+}
--- client.h
+++ client.h
@@ -2,6 +2,8 @@
 #define CLIENT_H
 
 #include <QWidget>
+#include <QDebug>
+#include <QTcpSocket>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class Client; }
@@ -15,7 +17,13 @@ public:
     Client(QWidget *parent = nullptr);
     ~Client();
 
+    void requestMessage();
+    void readMessage();
+    void showError();
+
 private:
     Ui::Client *ui;
+    QTcpSocket *clientSocket = nullptr;
+    //QDebug *q=nullptr;
 };
 #endif // CLIENT_H
